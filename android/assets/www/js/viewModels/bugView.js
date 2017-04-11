f6218988e3f659dac3139c14215c88232636cebc
Neo-Vincent <neo_vincent@outlook.com>
2017-04-11 21:26:27 +0800

    # merge bugView

--- android/assets/www/js/viewModels/bugView.js
+++ android/assets/www/js/viewModels/bugView.js
@@ -5,59 +5,67 @@
 /*
  * Your customer ViewModel code goes here
  */
-define(['ojs/ojcore', 'knockout', 'jquery', 'appController'],
- function(oj, ko, $, app) {
-  
-    function BugViewModel() {
-      var self = this;
-      // Header Config
-      var headerFactory = {
-           createViewModel: function(params, valueAccessor) {
-             var model =  {
-               pageTitle: app.router.currentState().label,
-               goBack : function() {
-                                          app.pendingAnimationType = 'navParent';
-                                          window.history.back();
-                                    },
-               handleBindingsApplied: function(info) {
-                 app.adjustContentPadding();
-               },
+define(['ojs/ojcore', 'knockout', 'jquery', 'appController', 'ojs/ojdatetimepicker', 'ojs/ojtimezonedata', 'ojs/ojinputtext', 'ojs/ojinputnumber', 'ojs/ojbutton', 'ojs/ojknockout'],
+    function (oj, ko, $, app) {
 
-             };
-             return Promise.resolve(model);
-           }
-       };
-      self.headerConfig = {'viewName': 'bugViewHeader', 'viewModelFactory':headerFactory };
+        function BugViewModel() {
+            var self = this;
+            // Header Config
+            var headerFactory = {
+                createViewModel: function (params, valueAccessor) {
+                    var model = {
+                        pageTitle: app.router.currentState().label,
+                        goBack: function () {
+                            app.pendingAnimationType = 'navParent';
+                            window.history.back();
+                        },
+                        handleBindingsApplied: function (info) {
+                            app.adjustContentPadding();
+                        },
+                        isEditMode: ko.observableArray([]),
+                        buttonClick: function (data, event) {
+                            console.log(event.currentTarget.id);
+                            return true;
+                        }
+                    };
+                    model.modeChange= ko.computed(function () {
+                        return  model.isEditMode().length ?console.log("edit") :console.log("read");
+                    }, model);
+                    return Promise.resolve(model);
+                }
+            };
+            self.headerConfig = {'viewName': 'bugViewHeader', 'viewModelFactory': headerFactory};
 
-      self.handleActivated = function(info) {
-        // Implement if needed
-      };
+            self.handleActivated = function (info) {
+                // Implement if needed
+            };
 
-      self.handleAttached = function(info) {
-        // Implement if needed
-      };
-
-
-      self.handleBindingsApplied = function(info) {
-        // Implement if needed
-      };
-
-
-      self.handleDetached = function(info) {
-        // Implement if needed
-      };
+            self.handleAttached = function (info) {
+                // Implement if needed
+            };
 
 
-      oj.Router.sync().then(
-          function() {
-             var id = app.router.retrieve();
-             app.router.store(null);
-          }
-       );
-        self.readMode=false;
-        self.status1="AA";
+            self.handleBindingsApplied = function (info) {
+                // Implement if needed
+            };
+
+
+            self.handleDetached = function (info) {
+                // Implement if needed
+            };
+
+            self.readMode = false;
+            self.status1 = "AA";
+            this.value = ko.observable("Somewhat long value");
+
+            oj.Router.sync().then(
+                function () {
+                    var id = app.router.retrieve();
+                    app.router.store(null);
+                }
+            );
+        }
+
+        return new BugViewModel();
     }
-
-    return new BugViewModel();
-  }
 );
